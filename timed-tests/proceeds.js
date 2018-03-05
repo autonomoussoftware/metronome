60c68d305b3476ed2a0b93c552366e45671835f3
manoj patidar <patidarmanoj@gmail.com>
2018-03-05 07:16:21 +0530

    remove ERC Claimable functions

--- timed-tests/proceeds.js
+++ timed-tests/proceeds.js
@@ -25,7 +25,6 @@
 
 const assert = require('chai').assert
 const ethjsABI = require('ethjs-abi')
-const ERCClaimableTests = require('../test/shared/erc-claimable')
 const MTNToken = artifacts.require('MTNToken')
 const SmartToken = artifacts.require('SmartToken')
 const Proceeds = artifacts.require('Proceeds')
@@ -246,7 +245,4 @@ contract('Proceeds - timed test', accounts => {
     })
   })
 
-  describe('claim airdropped erc tokens', () => {
-    ERCClaimableTests.tests(accounts, 'proceeds')
-  })
 })
