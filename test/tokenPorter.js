161802a0dae36cf13153851b7575451be1b8c7b3
Phillip Capiral <thefilcap@protonmail.com>
2018-03-14 11:37:53 -0700

    Add ability to export on-chain to a new MET token contract on ETH

--- test/tokenPorter.js
+++ test/tokenPorter.js
@@ -92,7 +92,7 @@ contract('TokenPorter', accounts => {
     })
   })
 
-  describe('export', () => {
+  describe('export off chain', () => {
     it('successful export', () => {
       return new Promise(async (resolve, reject) => {
         await initContracts(getCurrentBlockTime() - 60, MINIMUM_PRICE, STARTING_PRICE, TIME_SCALE)
@@ -107,7 +107,7 @@ contract('TokenPorter', accounts => {
         assert.isAbove(mtTokenBalanceBefore.toNumber(), 0, 'Buyer has no MET Tokens to export')
 
         // export all tokens tokens
-        const expectedDestChain = 'ETH'
+        const expectedDestChain = 'ETC'
         const expectedExtraData = 'extra data'
         const tx = await metToken.export(
           web3.fromAscii(expectedDestChain),
@@ -116,6 +116,9 @@ contract('TokenPorter', accounts => {
           mtTokenBalanceBefore,
           web3.fromAscii(expectedExtraData),
           { from: buyer })
+
+        const claimable = await tokenPorter.claimables(metToken.address, buyer)
+        assert.equal(claimable.toNumber(), 0, 'Claimable should not have been recorded')
 
         // check for burn
         assert.equal(tx.logs.length, 2, 'Incorrect number of logs emitted')
@@ -139,6 +142,7 @@ contract('TokenPorter', accounts => {
         assert.equal(destMetronomeAddr, metToken.address, 'Dest MetronomeAddr is different')
         const destinationRecipientAddr = logExportReceipt.destinationRecipientAddr
         assert.equal(destinationRecipientAddr, buyer, 'Dest Recipient is different')
+
         const extraData = logExportReceipt.extraData
         assert.equal(web3.toHex(extraData), web3.toHex(web3.fromAscii(expectedExtraData)), 'Extra Data is different')
         const currentTick = logExportReceipt.currentTick
@@ -170,6 +174,117 @@ contract('TokenPorter', accounts => {
         assert.equal(totalSupplyBefore.sub(totalSupplyAfter).toNumber(), amountToBurn, 'After export, total supply is not correct')
         assert.equal(mtTokenBalanceBefore.sub(mtTokenBalanceAfter).toNumber(), amountToBurn, 'After export, metTokenBalance is not correct')
         assert.equal(mtTokenBalanceAfter.toNumber(), 0, 'metTokenBalance after export should be zero')
+
+        resolve()
+      })
+    })
+  })
+
+  describe('export on chain', () => {
+    it('successful export', () => {
+      return new Promise(async (resolve, reject) => {
+        await initContracts(getCurrentBlockTime() - 60, MINIMUM_PRICE, STARTING_PRICE, TIME_SCALE)
+
+        // get some balance for export, half MET
+        const buyers = [accounts[7], accounts[8], accounts[9]]
+        const claims = []
+        const destMET = accounts[1]
+        for (let i = 0; i < buyers.length; i++) {
+          const buyer = accounts[i]
+          const amount = 1e18
+          await auctions.sendTransaction({ from: buyer, value: amount })
+
+          var totalSupplyBefore = await metToken.totalSupply()
+          var mtTokenBalanceBefore = await metToken.balanceOf(buyer)
+          assert.isAbove(mtTokenBalanceBefore.toNumber(), 0, 'Buyer has no MET Tokens to export')
+
+          // export all tokens tokens
+          const expectedDestChain = 'ETH'
+          const expectedExtraData = 'extra data'
+          const tx = await metToken.export(
+            web3.fromAscii(expectedDestChain),
+            destMET,
+            buyer,
+            mtTokenBalanceBefore,
+            web3.fromAscii(expectedExtraData),
+            { from: buyer })
+
+          // check for burn
+          assert.equal(tx.logs.length, 2, 'Incorrect number of logs emitted')
+          const burnLog = tx.logs[1]
+          assert.equal(burnLog.event, 'Transfer', 'Burn was not emitted')
+          assert.equal(burnLog.args._from, buyer, 'From is wrong')
+          assert.equal(burnLog.args._to, 0x0, 'To is wrong')
+          assert.equal(burnLog.args._value.toNumber(), mtTokenBalanceBefore.toNumber(), 'Value is wrong')
+
+          const claimable = await tokenPorter.claimables(destMET, buyer)
+          assert.equal(claimable.toNumber(), mtTokenBalanceBefore.toNumber(), 'Claimable was not recorded')
+
+          // check for export receipt
+          const decoder = ethjsABI.logDecoder(tokenPorter.abi)
+          const tokenPorterEvents = decoder(tx.receipt.logs)
+          assert.equal(tokenPorterEvents.length, 1, 'Incorrect number of logs emitted')
+          const logExportReceipt = tokenPorterEvents[0]
+          assert.equal(logExportReceipt._eventName, 'ExportReceiptLog', 'Log name is wrong')
+          const amountToBurn = parseInt(logExportReceipt.amountToBurn.toString(), 10)
+          assert.equal(amountToBurn, mtTokenBalanceBefore.toNumber(), 'Amounts are different')
+          const destinationChain = logExportReceipt.destinationChain
+          assert.equal(web3.toHex(destinationChain), web3.toHex(web3.fromAscii(expectedDestChain)) + '0000000000', 'Dest Chain is different')
+          const destMetronomeAddr = logExportReceipt.destinationMetronomeAddr
+          assert.equal(destMetronomeAddr, destMET, 'Dest MetronomeAddr is different')
+          const destinationRecipientAddr = logExportReceipt.destinationRecipientAddr
+          assert.equal(destinationRecipientAddr, buyer, 'Dest Recipient is different')
+          claims.push({ address: destinationRecipientAddr, amount: amountToBurn })
+
+          const extraData = logExportReceipt.extraData
+          assert.equal(web3.toHex(extraData), web3.toHex(web3.fromAscii(expectedExtraData)), 'Extra Data is different')
+          const currentTick = logExportReceipt.currentTick
+          assert.equal(currentTick.toNumber(), (await auctions.currentTick()).toNumber(), 'Current Tick is different')
+          const burnSequence = logExportReceipt.burnSequence
+          assert.equal(burnSequence.toNumber(), i + 1, 'burnSequence is different')
+
+          // TODO: is there a way to validate without an import?
+          var totalSupplyAfter = await metToken.totalSupply()
+          const currentBurnHash = logExportReceipt.currentBurnHash
+          assert.isNotEmpty(currentBurnHash, 'Burn Hash is empty')
+          const prevBurnHash = logExportReceipt.prevBurnHash
+          assert.isNotEmpty(prevBurnHash, 'Prev Burn Hash is empty')
+          const dailyMintable = logExportReceipt.dailyMintable
+          assert.isNotEmpty(dailyMintable, 'Prev Burn Hash is empty')
+          const supplyOnAllChains = logExportReceipt.supplyOnAllChains
+          assert.equal(supplyOnAllChains.length, 6, 'Supply On All Chains is wrong length')
+          assert.equal(parseInt(supplyOnAllChains[0].toString(), 10), totalSupplyAfter.toNumber(), 'First chain is not non-zero')
+          assert.equal(supplyOnAllChains[1].toString(), '0', 'First chain is not non-zero')
+          assert.equal(supplyOnAllChains[2].toString(), '0', 'First chain is not zero')
+          assert.equal(supplyOnAllChains[3].toString(), '0', 'First chain is not zero')
+          assert.equal(supplyOnAllChains[4].toString(), '0', 'First chain is not zero')
+          assert.equal(supplyOnAllChains[5].toString(), '0', 'First chain is not zero')
+
+          assert.isAbove(logExportReceipt.genesisTime.toNumber(), 0, 'genesisTime is wrong')
+
+          // reconcile balances
+          var mtTokenBalanceAfter = await metToken.balanceOf(buyer)
+          assert.equal(totalSupplyBefore.sub(totalSupplyAfter).toNumber(), amountToBurn, 'After export, total supply is not correct')
+          assert.equal(mtTokenBalanceBefore.sub(mtTokenBalanceAfter).toNumber(), amountToBurn, 'After export, metTokenBalance is not correct')
+          assert.equal(mtTokenBalanceAfter.toNumber(), 0, 'metTokenBalance after export should be zero')
+        }
+
+        const count = await tokenPorter.claimReceivables.call(claims.map(c => c.address), {from: destMET})
+        assert.equal(count.toNumber(), claims.length, 'Not all claims were processed')
+
+        const tx = await tokenPorter.claimReceivables(claims.map(c => c.address), {from: destMET})
+        assert.equal(tx.logs.length, claims.length, 'Not all claim logs were emitted')
+
+        for (let i = 0; i < claims.length; i++) {
+          const claim = claims[i]
+          const log = tx.logs[i]
+          assert.equal(log.args.destinationMetronomeAddr, destMET, 'Destination MET address does not match')
+          assert.equal(log.args.destinationRecipientAddr, claim.address, 'Recipient address does not match')
+          assert.equal(log.args.amount.toNumber(), claim.amount, 'Burned amount is not correct')
+
+          const claimableAfter = await tokenPorter.claimables(destMET, claim.address)
+          assert.equal(claimableAfter.toNumber(), 0, 'Claimable was not set to zero')
+        }
 
         resolve()
       })
