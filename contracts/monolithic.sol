0185e56442f17892cadfe127b9f4f2130660aeec
Manoj Patidar <patidarmanoj@gmail.com>
2018-12-29 18:01:05 -0800

    updated copyright text

--- contracts/monolithic.sol
+++ contracts/monolithic.sol
@@ -1,7 +1,7 @@
 /*
     The MIT License (MIT)
 
-    Copyright 2017 - 2018, Alchemy Limited, LLC and Smart Contract Solutions.
+    Copyright 2018 - 2019, Autonomous Software.
 
     Permission is hereby granted, free of charge, to any person obtaining
     a copy of this software and associated documentation files (the
--- js/etc/initMetronome.js
+++ js/etc/initMetronome.js
@@ -1,7 +1,7 @@
 /*
     The MIT License (MIT)
 
-    Copyright 2017 - 2018, Alchemy Limited, LLC.
+    Copyright 2018 - 2019, Autonomous Software.
 
     Permission is hereby granted, free of charge, to any person obtaining
     a copy of this software and associated documentation files (the
