2e4e938b570062db0263f869aa10b19f9ba52c3b
patidarmanoj10 <32006767+patidarmanoj10@users.noreply.github.com>
2019-04-22 20:09:08 -0700

    ETH- A new contract to allow validators to add/remove validator (#80)
    Proposal contracs. Validator can vote to add/remove validators, update threshold.

--- contracts/monolithic.sol
+++ contracts/monolithic.sol
@@ -1953,6 +1953,176 @@ contract TokenPorter is ITokenPorter, Owned {
 }    
 
 
+/// @title Proposal intiated by validators.  
+contract Proposals is Owned {
+    uint public votingPeriod = 60 * 60 * 24 * 15;
+
+    Validator public validator;
+
+    bytes32[] public actions;
+    
+    struct Proposal {
+        uint proposalId;
+        bytes32 action;
+        uint expiry;
+        address validator;
+        uint newThreshold;
+        uint supportCount;
+        address[] voters;
+        bool passed;
+        mapping (address => bool) voted;
+
+    }
+
+    Proposal[] public proposals;
+
+    event LogVoted(uint indexed proposalId, address indexed voter, bool support);
+
+    event LogProposalCreated(uint indexed proposalId, address indexed newValidator, 
+        uint newThreshold, address creator, uint expiry, bytes32 indexed action);
+
+    event LogProposalClosed(uint indexed proposalId, address indexed newValidator, 
+        uint newThreshold, bytes32 indexed action, uint expiry, uint supportCount, bool passed);
+
+    /// @dev Throws if called by any account other than the validator.
+    modifier onlyValidator() {
+        require(validator.isValidator(msg.sender));
+        _;
+    }
+
+    constructor() public {
+        actions.push("addval");
+        actions.push("removeval");
+        actions.push("updatethreshold");
+    }
+
+    /// @notice set address of validator contract
+    /// @param _validator address of validator contract
+    function setValidator(address _validator) public onlyOwner returns (bool) {
+        require(_validator != 0x0);
+        validator = Validator(_validator);
+        return true;
+    }
+
+    /// @notice set update voting period
+    /// @param _t voting period
+    function updateVotingPeriod(uint _t) public onlyOwner returns (bool) {
+        require(_t != 0);
+        votingPeriod = _t;
+        return true;
+    }
+
+    /// @notice validator can initiate proposal for add new validator.
+    /// @param _validator new validator address
+    /// @param _newThreshold new threshold value. 0 if do not want to update it
+    function proposeNewValidator(address _validator, uint _newThreshold) public onlyValidator returns (uint) {
+        require(_validator != 0x0);
+        require(!validator.isValidator(_validator));
+        if (_newThreshold > 0) {
+            uint valCount = validator.getValidatorsCount();
+            require(validator.isNewThresholdValid(valCount + 1, _newThreshold));
+        }
+        return createNewProposal(_validator, msg.sender, actions[0], _newThreshold);
+    }
+
+    /// @notice validator can initiate proposal to remove bad actor or idle validators.
+    /// validators can be removed if support count >= threshold or  support count == voting count.
+    /// Later approach is to remove idle validator from system. 
+    /// @param _validator new validator address
+    /// @param _newThreshold new threshold value. 0 if do not want to update it
+    function proposeRemoveValidator(address _validator, uint _newThreshold) public onlyValidator returns (uint) {
+        require(_validator != 0x0);
+        require(validator.isValidator(_validator));
+        if (_newThreshold > 0) {
+            uint valCount = validator.getValidatorsCount();
+            require(validator.isNewThresholdValid(valCount - 1, _newThreshold));
+        }
+        return createNewProposal(_validator, msg.sender, actions[1], _newThreshold);
+    }
+
+    /// @notice validator can initiate proposal to update threshold value
+    /// @param _newThreshold new threshold value. 0 if do not want to update it
+    function proposeNewThreshold(uint _newThreshold) public onlyValidator returns (uint) {
+        uint valCount = validator.getValidatorsCount();
+        require(validator.isNewThresholdValid(valCount, _newThreshold));
+        return createNewProposal(0x0, msg.sender, actions[2], _newThreshold);
+    }
+
+    /// @notice validator can vote for a proposal
+    /// @param _proposalId ..
+    /// @param _support true/false
+    function voteForProposal(uint _proposalId, bool _support) public onlyValidator {
+        require(proposals[_proposalId].expiry != 0);
+        require(now < proposals[_proposalId].expiry);
+        require(!proposals[_proposalId].passed);
+        require(!(proposals[_proposalId]).voted[msg.sender]);
+        proposals[_proposalId].voters.push(msg.sender);
+        proposals[_proposalId].voted[msg.sender] = true;
+        if (_support) {
+            proposals[_proposalId].supportCount++;
+        }
+        emit LogVoted(_proposalId, msg.sender, _support);
+    }
+    
+    /// @notice public function to close a proposal if expired or majority support received
+    /// @param _proposalId ..
+    function closeProposal(uint _proposalId) public {
+        require(proposals[_proposalId].expiry != 0);
+        if (proposals[_proposalId].supportCount >= validator.threshold()) {
+            executeProposal(_proposalId, proposals[_proposalId].newThreshold);
+        } else if (now > proposals[_proposalId].expiry) {
+            // Proposal to remove idle validator if no one take objection
+            if ((proposals[_proposalId].action == actions[1]) && 
+                (proposals[_proposalId].voters.length == proposals[_proposalId].supportCount)) {
+                // new threshold count should never go below 50% remaining validators
+                uint _t = (validator.getValidatorsCount() + 1) / 2;
+                uint newThreshold = validator.threshold() - 1;
+                if (newThreshold < _t) {
+                    newThreshold = _t;
+                }
+                executeProposal(_proposalId, newThreshold);
+            }
+        }   
+    }
+
+    /// @notice private function to update outcome of a proposal
+    /// @param _proposalId ..
+    /// @param _newThreshold ..
+    function executeProposal(uint _proposalId, uint _newThreshold) private {
+        if (proposals[_proposalId].action == actions[0]) {
+            validator.addValidator(proposals[_proposalId].validator);
+        } else if (proposals[_proposalId].action == actions[1]) {
+            validator.removeValidator(proposals[_proposalId].validator);
+        }
+        if (_newThreshold != 0) {
+            validator.updateThreshold(_newThreshold);
+        }
+        proposals[_proposalId].passed = true;
+        emit LogProposalClosed(_proposalId, proposals[_proposalId].validator, 
+            _newThreshold, proposals[_proposalId].action, proposals[_proposalId].expiry, 
+            proposals[_proposalId].supportCount, true);
+    }
+
+    /// @notice private function to create a proposal
+    /// @param _validator validator address
+    /// @param _creator creator
+    /// @param _action _action
+    /// @param _newThreshold _newThreshold
+    function createNewProposal(address _validator, address _creator, bytes32 _action, 
+        uint _newThreshold) private returns (uint proposalId) {
+        proposalId = proposals.length++;
+        uint expiry = now + votingPeriod;
+        Proposal storage p = proposals[proposalId];
+        p.proposalId = proposalId;
+        p.action = _action;
+        p.expiry = expiry;
+        p.validator = _validator;
+        p.newThreshold = _newThreshold;
+        emit LogProposalCreated(proposalId, _validator, _newThreshold, _creator, expiry, _action);
+    }
+}
+
+
 /// @title Validator contract for off chain validators to validate hash
 contract Validator is Owned {
 
@@ -1967,24 +2137,38 @@ contract Validator is Owned {
     METToken public token;
     TokenPorter public tokenPorter;
     Auctions public auctions;
+    Proposals public proposals;
 
     mapping (bytes32 => bool) public hashClaimed;
 
-    uint public threshold = 2;
+    // Miniumum quorum require for various voting like import, add new validators, add new chain
+    uint public threshold = 1;
 
     event LogAttestation(bytes32 indexed hash, address indexed recipientAddr, bool isValid);
+        
+    /// @dev Throws if called by any account other than the validator.
+    modifier onlyValidator() {
+        require(isValidator[msg.sender]);
+        _;
+    }
+
+    /// @dev Throws if called by unauthorized account
+    modifier onlyAuthorized() {
+        require(msg.sender == owner || msg.sender == address(proposals));
+        _;
+    }
 
     /// @param _validator validator address
-    function addValidator(address _validator) public onlyOwner {
+    function addValidator(address _validator) public onlyAuthorized {
         require(!isValidator[_validator]);
         validators.push(_validator);
         isValidator[_validator] = true;
     }
 
     /// @param _validator validator address
-    function removeValidator(address _validator) public onlyOwner {
-        // Must add new validators before removing to maintain minimum three validators active
-        require(validators.length > 3);
+    function removeValidator(address _validator) public onlyAuthorized {
+        // Must add new validators before removing to maintain minimum one validator active
+        require(validators.length > 1);
         delete isValidator[_validator];
         for (uint i = 0; i < (validators.length); i++) {
             if (validators[i] == _validator) {
@@ -1994,7 +2178,15 @@ contract Validator is Owned {
                 validators.length--; 
                 break;
             }
-        }  
+        }
+
+        if (threshold >= validators.length) {
+            if (validators.length == 1) {
+                threshold = 1;
+            } else {
+                threshold = validators.length - 1;
+            }
+        }
     }
 
     /// @notice fetch count of validators
@@ -2005,11 +2197,31 @@ contract Validator is Owned {
     /// @notice set threshold for validation and minting
     /// @param _threshold threshold count
     /// @return true/false
-    function updateThreshold(uint _threshold) public onlyOwner returns (bool) {
-        require(_threshold > 1);
-        require(_threshold <= validators.length);
-        require(_threshold > validators.length / 2);
+    function updateThreshold(uint _threshold) public onlyAuthorized returns (bool) {
+        require(isNewThresholdValid(validators.length, _threshold));
         threshold = _threshold;
+        return true;
+    }
+
+    /// @notice check valid threshold value. Common function for validator and proposal contract
+    /// @param _valCount valicator count
+    /// @param _threshold new threshold value
+    /// @return true/false
+    function isNewThresholdValid(uint _valCount, uint _threshold) public pure returns (bool) {
+        if (_threshold == 1 && _valCount == 2) {
+            return true;
+        } else if (_threshold >= 1 && _threshold < _valCount && (_threshold > (_valCount / 2))) {
+            return true;
+        }
+        return false;
+    }
+
+    /// @notice set address of Proposals contract
+    /// @param _proposals address of token porter
+    /// @return true/false
+    function setProposalContract(address _proposals) public onlyOwner returns (bool) {
+        require(_proposals != 0x0);
+        proposals = Proposals(_proposals);
         return true;
     }
 
@@ -2046,8 +2258,7 @@ contract Validator is Owned {
     /// @param _globalSupplyInOtherChains total supply in all other chains except this chain
     function attestHash(bytes32 _burnHash, bytes8 _originChain, address _recipientAddr, 
         uint _amount, uint _fee, bytes32[] _proof, bytes _extraData,
-        uint _globalSupplyInOtherChains) public {
-        require(isValidator[msg.sender]);
+        uint _globalSupplyInOtherChains) public onlyValidator {
         require(_burnHash != 0x0);
         require(!hashAttestations[_burnHash][msg.sender]);
         require(!hashRefutation[_burnHash][msg.sender]);
@@ -2065,8 +2276,7 @@ contract Validator is Owned {
 
     /// @notice off chain validator can refute hash, if given export hash is not verified in origin chain.
     /// @param _burnHash Burn hash
-    function refuteHash(bytes32 _burnHash, address _recipientAddr) public {
-        require(isValidator[msg.sender]);
+    function refuteHash(bytes32 _burnHash, address _recipientAddr) public onlyValidator {
         require(!hashAttestations[_burnHash][msg.sender]);
         require(!hashRefutation[_burnHash][msg.sender]);
         hashRefutation[_burnHash][msg.sender] = true;
